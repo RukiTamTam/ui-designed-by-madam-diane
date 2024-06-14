4e0cd4c4e894afd69923fd5c1b6c25c659067320
RukiTamTam <wcurimatmat24@gmail.com>
2024-06-14 23:00:07 +0800

    Styling for mobile view complete

--- Day 2 - Credit Card Checkout/script.js
+++ Day 2 - Credit Card Checkout/script.js
@@ -1,22 +1,13 @@
-function labelOffset() {
-	let labels = document.getElementsByClassName("js-form__label");
-	let textInputs = document.getElementsByClassName("js-form__text-input");
+let labels = document.querySelectorAll(".js-form__label");
+let textInputs = document.querySelectorAll(".js-form__text-input");
 
-	Array.from(textInputs).forEach((input, index) => {
-		let label = labels[index];
-
-		input.addEventListener("focus", () => {
-			label.style.transform = "translateY(-20px)";
-			label.style.zIndex = "3";
-		});
-
-		input.addEventListener("blur", () => {
-			if (input.value === "") {
-				label.style.transform = "translateY(0)";
-				label.style.zIndex = "";
-			}
-		});
+textInputs.forEach((input, index) => {
+	input.addEventListener("focus", () => {
+		labels[index].style.transform = "translateY(-20px)";
+		labels[index].style.zIndex = "3";
 	});
-}
-
-labelOffset();
+	input.addEventListener("blur", () => {
+		labels[index].style.transform = "translateY(0)";
+		labels[index].style.zIndex = "1";
+	});
+});
